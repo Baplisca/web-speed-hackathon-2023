d51d93415159d59d2dbb37906e1b852eea5fefa6
Baplisca <yokota0327@gmail.com>
2023-03-05 11:52:00 +0900

    delete font load

--- src/client/pages/NotFound/NotFound.tsx
+++ src/client/pages/NotFound/NotFound.tsx
@@ -1,27 +1,14 @@
 import type { FC } from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { Layout } from '../../components/application/Layout';
-import { loadFonts } from '../../utils/load_fonts';
 
 import * as styles from './NotFound.styles';
 
 export const NotFound: FC = () => {
-  const [isReady, setIsReady] = useState(false);
-
   useEffect(() => {
-    const load = async () => {
-      await loadFonts();
-      setIsReady(true);
-    };
-
-    load();
     document.title = 'ページが見つかりませんでした';
   }, []);
-
-  if (!isReady) {
-    return null;
-  }
 
   return (
     <>
--- src/client/utils/load_fonts.ts
+++ src/client/utils/load_fonts.ts
@@ -1,40 +1,44 @@
-type FontFaceSource = {
+interface FontFaceDescriptors {
+  display?: string;
+  style?: string;
+  weight?: string;
+  stretch?: string;
+  unicodeRange?: string;
+  variant?: string;
+  featureSettings?: string;
+}
+
+interface FontFaceSource {
   family: string;
   source: string;
-  descripter: FontFaceDescriptors;
-};
+  descriptors?: FontFaceDescriptors;
+}
 
 const FONT_FACE_SOURCES: FontFaceSource[] = [
   {
-    descripter: {
-      display: 'block',
-      style: 'normal',
-      weight: '700',
+    family: 'Noto Sarif JP',
+    source: 'https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400&display=swap',
+    descriptors: {
+      display: 'swap',
     },
-    family: 'Noto Serif JP',
-    source: "url('/fonts/NotoSerifJP-Bold.otf')",
   },
   {
-    descripter: {
-      display: 'block',
-      style: 'normal',
-      weight: '400',
+    family: 'Noto Sarif JP',
+    source: 'https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&display=swap',
+    descriptors: {
+      display: 'swap',
+      weight: '700',
     },
-    family: 'Noto Serif JP',
-    source: "url('/fonts/NotoSerifJP-Regular.otf')",
   },
 ];
 
 export async function loadFonts() {
-  const fontFaces = FONT_FACE_SOURCES.map(({ descripter, family, source }) => new FontFace(family, source, descripter));
-  const fonts: FontFace[] = [];
-
-  for (const fontFace of fontFaces) {
-    const font = await fontFace.load();
-    fonts.push(font);
-  }
-
-  for (const font of fontFaces) {
-    document.fonts.add(font);
-  }
+  const fonts = await Promise.all(
+    FONT_FACE_SOURCES.map((fontFaceSource) => {
+      const fontFace = new FontFace(fontFaceSource.family, fontFaceSource.source, fontFaceSource.descriptors);
+      document.fonts.add(fontFace);
+      return fontFace.load();
+    }),
+  );
+  console.log('Fonts loaded:', fonts);
 }
