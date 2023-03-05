47c8e6f384b4db42c317a72e94b9c877b6e83f63
Baplisca <yokota0327@gmail.com>
2023-03-05 10:35:00 +0900

    delete video preload and vite video setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
 import wasm from 'vite-plugin-wasm';
 
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
-
 export default defineConfig(async () => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     plugins: [
       react(),
@@ -29,7 +13,6 @@ export default defineConfig(async () => {
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        videos,
       }),
     ],
   };
